7554149fdf981c9dca9bf892d9efec2584463b06
codehunterzahid <codehuntingwithzahid@gmail.com>
2026-01-05 17:56:32 +0500

    App.js formated just clearing changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,19 @@
-import { BrowserRouter } from 'react-router-dom'
-import './App.css'
-import AppRoutes from './routes/Routes'
-import HomeRoute from './routes/HomeRoute'
-import UserRoute from './routes/UserRoute'
-import SalonsRoute from './routes/SalonsRoute'
-import AdminRoute from './routes/AdminRoute'
-import ScrollToTop from './utils/ScrollToTop'
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import AppRoutes from "./routes/Routes";
+import HomeRoute from "./routes/HomeRoute";
+import UserRoute from "./routes/UserRoute";
+import SalonsRoute from "./routes/SalonsRoute";
+import AdminRoute from "./routes/AdminRoute";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
-
   return (
-  
-  <BrowserRouter>
-    <ScrollToTop />
-    <HomeRoute />
-  </BrowserRouter>
-  
-  
-  
-  )
+    <BrowserRouter>
+      <ScrollToTop />
+      <HomeRoute />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
